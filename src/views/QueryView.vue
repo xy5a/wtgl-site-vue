42d7ae5c22a4e60965c7a21b44bdf91d2bfabb10
xy_5a <xy_5a@outlook.com>
2023-12-03 21:27:37 +0800

    制作query view

--- src/views/QueryView.vue
+++ src/views/QueryView.vue
@@ -1,5 +1,59 @@
-<script setup></script>
+<script setup>
+import { ElButton, ElTable, ElTableColumn } from 'element-plus'
+
+let data = [
+  {
+    id: 1,
+    name: 'John Brown',
+    collegeName: 'College of Engineering',
+    identity: 'captain',
+    email: 'john@gmail.com',
+    qq: '123456789',
+    wechat: '123456789',
+    phone: '123456789'
+  },
+  {
+    id: 2,
+    name: 'Jim Green',
+    collegeName: 'College of Engineering',
+    identity: 'partner',
+    email: 'jim@gmail.com',
+    qq: '123456789',
+    wechat: '123456789',
+    phone: '1234599999'
+  },
+  {
+    id: 3,
+    name: 'Joe Black',
+    collegeName: 'College of Engineering',
+    identity: 'partner',
+    email: 'joe@gmail.com',
+    qq: '123456789',
+    wechat: '123456789',
+    phone: '1234599999'
+  }
+]
+</script>
 
 <template>
-  <h1>Query view here</h1>
+  <ElTable :data="data">
+    <ElTableColumn prop="name" label="姓名"></ElTableColumn>
+    <ElTableColumn prop="collegeName" label="学校"></ElTableColumn>
+    <ElTableColumn prop="identity" label="身份"></ElTableColumn>
+    <ElTableColumn fixed="right" label="操作">
+      <template #default="scope">
+        <ElButton
+          type="primary"
+          size="small"
+          @click="
+            () => {
+              console.log(scope.$index)
+              console.log(`modify id: ${data[scope.$index].id}`)
+            }
+          "
+          >编辑</ElButton
+        >
+      </template>
+    </ElTableColumn>
+  </ElTable>
 </template>
